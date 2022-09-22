7ac15c48924e46813c98ca5a3df0269d8fdd1394
Sanmoy Karmakar <skarmaka@andrew.cmu.edu>
2022-09-22 13:35:06 -0400

    html changes

--- src/main/java/com/develogical/web/IndexPage.java
+++ src/main/java/com/develogical/web/IndexPage.java
@@ -8,7 +8,7 @@ public class IndexPage extends HtmlPage {
     protected void writeContentTo(PrintWriter writer) {
         writer.println(
                 "<h1>Welcome 17-313 CMU User!</h1>" +
-                "<h1>This is an HTML change </h1>" +
+                "<h1>This is an HTML change to check if delpoyed</h1>" +
                 "<p>Enter your query in the box below: " +
                   "<form><input type=\"text\" name=\"q\" />" +
                     "<input type=\"submit\">" +
